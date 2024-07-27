3dbff5e2f8db45923c24556f2b7517a86f56a7eb
Leo Zeng <qingchun1995@gmail.com>
2024-07-27 19:53:11 +0800

    Construct the classes Motorcycle and Truck with methods

--- src/models/Motorcycle.java
+++ src/models/Motorcycle.java
@@ -1,4 +1,62 @@
 package models;
 
-public class Motorcycle {
+import interfaces.MotorVehicle;
+
+public class Motorcycle implements MotorVehicle {
+
+    /**
+     * @param wheels
+     */
+    @Override
+    public void setNumberOfWheels(int wheels) {
+
+    }
+
+    /**
+     * @return
+     */
+    @Override
+    public int getNumberOfWheels() {
+        return 0;
+    }
+
+    /**
+     * @param motorcycleType
+     */
+    @Override
+    public void setMotorcycleType(String motorcycleType) {
+
+    }
+
+    /**
+     * @return
+     */
+    @Override
+    public String getMotorcycleType() {
+        return "";
+    }
+
+    /**
+     * @return
+     */
+    @Override
+    public String getMake() {
+        return "";
+    }
+
+    /**
+     * @return
+     */
+    @Override
+    public String getModel() {
+        return "";
+    }
+
+    /**
+     * @return
+     */
+    @Override
+    public int getYear() {
+        return 0;
+    }
 }
--- src/models/Truck.java
+++ src/models/Truck.java
@@ -1,4 +1,61 @@
 package models;
 
-public class Truck {
+import interfaces.TruckVehicle;
+
+public class Truck implements TruckVehicle {
+    /**
+     * @param capacity
+     */
+    @Override
+    public void setCargoCapacity(double capacity) {
+
+    }
+
+    /**
+     * @return
+     */
+    @Override
+    public double getCargoCapacity() {
+        return 0;
+    }
+
+    /**
+     * @param transmissionType
+     */
+    @Override
+    public void setTransmissionType(String transmissionType) {
+
+    }
+
+    /**
+     * @return
+     */
+    @Override
+    public String getTransmissionType() {
+        return "";
+    }
+
+    /**
+     * @return
+     */
+    @Override
+    public String getMake() {
+        return "";
+    }
+
+    /**
+     * @return
+     */
+    @Override
+    public String getModel() {
+        return "";
+    }
+
+    /**
+     * @return
+     */
+    @Override
+    public int getYear() {
+        return 0;
+    }
 }
