5bf05eba52c9686b446e10c43f0d6cec5b5f5ce6
Leo Zeng <qingchun1995@gmail.com>
2024-07-27 19:34:30 +0800

    Initialize an interface named MotorVehicle that has methods for setting and retrieving the number of wheels and the type of motorcycle

---
+++ src/MotorVehicle.java
@@ -0,0 +1,8 @@
+public interface MotorVehicle extends Vehicle {
+    // interface methods
+    void setNumberOfWheels(int wheels);
+    int getNumberOfWheels();
+
+    void setMotorcycleType(String motorcycleType);
+    String getMotorcycleType();
+}
