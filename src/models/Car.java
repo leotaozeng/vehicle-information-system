c6d17553e1d6652f0983432379701859493a563b
Leo Zeng <qingchun1995@gmail.com>
2024-07-28 16:42:14 +0800

    Construct the class Car that implements the Vehicle and CarVehicle interfaces

--- src/models/Car.java
+++ src/models/Car.java
@@ -3,59 +3,72 @@ package models;
 import interfaces.CarVehicle;
 
 public class Car implements CarVehicle {
+    private String make;
+    private String model;
+    private int year;
+    private int numberOfDoors;
+    private String fuelType;
+
+    // Create a class constructor for the Car class
+    public Car(String make, String model, int year) {
+        this.make = make;
+        this.model = model;
+        this.year = year;
+    }
+
+    /**
+     * @return make - 车辆品牌
+     */
+    @Override
+    public String getMake() {
+        return make;
+    }
+
+    /**
+     * @return model - 车辆模型
+     */
+    @Override
+    public String getModel() {
+        return model;
+    }
+
+    /**
+     * @return year - 生产年份
+     */
+    @Override
+    public int getYear() {
+        return year;
+    }
+
     /**
      * @param doors - Number of doors
      */
     @Override
     public void setNumberOfDoors(int doors) {
-
+        this.numberOfDoors = doors;
     }
 
     /**
-     * @return
+     * @return numberOfDoors
      */
     @Override
     public int getNumberOfDoors() {
-        return 0;
+        return numberOfDoors;
     }
 
     /**
-     * @param fuel
+     * @param fuelType - Type of fuel (petrol, diesel, or electric)
      */
     @Override
-    public void setFuelType(String fuel) {
-
+    public void setFuelType(String fuelType) {
+        this.fuelType = fuelType;
     }
 
     /**
-     * @return
+     * @return fuelType
      */
     @Override
     public String getFuelType() {
-        return "";
-    }
-
-    /**
-     * @return
-     */
-    @Override
-    public String getMake() {
-        return "";
-    }
-
-    /**
-     * @return
-     */
-    @Override
-    public String getModel() {
-        return "";
-    }
-
-    /**
-     * @return
-     */
-    @Override
-    public int getYear() {
-        return 0;
+        return fuelType;
     }
 }
