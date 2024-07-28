17b09df94cdfd81562f91cd1f9db7d9925a21b97
Leo Zeng <qingchun1995@gmail.com>
2024-07-28 17:04:40 +0800

    Construct the class Truck that implements the Vehicle and TruckVehicle interfaces

--- src/models/Truck.java
+++ src/models/Truck.java
@@ -3,59 +3,72 @@ package models;
 import interfaces.TruckVehicle;
 
 public class Truck implements TruckVehicle {
-    /**
-     * @param capacity
-     */
-    @Override
-    public void setCargoCapacity(double capacity) {
+    private String make;
+    private String model;
+    private int year;
+    private double cargoCapacity;
+    private String transmissionType;
 
+    // Create a class constructor for the Truck class
+    public Truck(String make, String model, int year) {
+        this.make = make;
+        this.model = model;
+        this.year = year;
     }
 
     /**
-     * @return
-     */
-    @Override
-    public double getCargoCapacity() {
-        return 0;
-    }
-
-    /**
-     * @param transmissionType
-     */
-    @Override
-    public void setTransmissionType(String transmissionType) {
-
-    }
-
-    /**
-     * @return
-     */
-    @Override
-    public String getTransmissionType() {
-        return "";
-    }
-
-    /**
-     * @return
+     * @return make - 车辆品牌
      */
     @Override
     public String getMake() {
-        return "";
+        return make;
     }
 
     /**
-     * @return
+     * @return model - 车辆模型
      */
     @Override
     public String getModel() {
-        return "";
+        return model;
     }
 
     /**
-     * @return
+     * @return year - 生产年份
      */
     @Override
     public int getYear() {
-        return 0;
+        return year;
+    }
+
+    /**
+     * @param capacity - Cargo capacity (in tons)
+     */
+    @Override
+    public void setCargoCapacity(double capacity) {
+        this.cargoCapacity = capacity;
+    }
+
+    /**
+     * @return cargoCapacity
+     */
+    @Override
+    public double getCargoCapacity() {
+        return cargoCapacity;
+    }
+
+    /**
+     * @param transmissionType - Type of transmission (manual or automatic)
+     */
+    @Override
+    public void setTransmissionType(String transmissionType) {
+        this.transmissionType = transmissionType;
+    }
+
+    /**
+     * @return transmissionType
+     */
+    @Override
+    public String getTransmissionType() {
+        return transmissionType;
     }
 }
